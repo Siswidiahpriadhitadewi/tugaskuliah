b9789588cc2cf20ba2229358eb25fcba6e624610
unknown <siswidiahpriadhitadewi>
2021-03-31 06:41:54 -0700

    commited

--- PERTEMUAN7/Praktikum/praktikum7/src/nomer1_2/MahasiswaMain.java
+++ PERTEMUAN7/Praktikum/praktikum7/src/nomer1_2/MahasiswaMain.java
@@ -11,46 +11,56 @@ package nomer1_2;
  */
 import java.util.Scanner;
 public class MahasiswaMain {
-    public static void main(String[] args) {
+   public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         Scanner s1 = new Scanner(System.in);
-        
+
         PencarianMhs data = new PencarianMhs();
-        int jumMhs = 5;
-        
-        System.out.println("~~~~~~~~~~~~~~~~~~");
-        System.out.println("MASUKKAN DATA MAHASISSWA SECARA URUT dari NIM TERKECIL");
-        for (int i = 0; i < jumMhs; i++) {
-            System.out.println("~~~~~~");
-            System.out.print("NIM \t  = ");
+
+        System.out.println("-----------------------------------------------------");
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        data.pjgArr = s.nextInt();
+
+        System.out.println("Masukkan data mahasiswa :");
+        for (int i = 0; i < data.pjgArr; i++) {
+            System.out.println("------------------");
+            System.out.print("NIM \t: ");
             int nim = s.nextInt();
-            System.out.print("NAMA \t  = ");
+            System.out.print("Nama \t: ");
             String nama = s1.nextLine();
-            System.out.print("UMUR \t  = ");
+            System.out.print("Umur \t: ");
             int umur = s.nextInt();
-            System.out.print("IPK \t  = ");
+            System.out.print("Ipk \t: ");
             double ipk = s.nextDouble();
-            
-            Mahasiswa m = new Mahasiswa(nim, nama, umur, ipk);
+
+            Mahasiswa m = new Mahasiswa(nim, nama, umur,
+                    ipk);
             data.tambah(m);
         }
-            
-            System.out.println("~~~~~~~~~~~~~~~~~~");
-            System.out.println("DATA KESELURUHAN MAHASISWA  = ");
-            data.tampil();
-            
-            System.out.println("~~~~~~~~~~~~~~~~~~");
-            System.out.println("~~~~~~~~~~~~~~~~~~");
-            System.out.println("~~~PENCARIAN DATA~~~");
-            System.out.println("~~~~~~~~~~~~~~~~~~");
-            System.out.println("Masukkan Nim Mahasiswa yang dicari");
-            System.out.print("NIM     = ");
-            int cari = s.nextInt();
-            System.out.println("MENGGUNAKAN SEQUANTIAL SEARCH Sequantial Search");
-            int posisi = data.FindSeqSearch(cari);
-            
-            data.TampilPosisi(cari, posisi);
-            data.TampilData(cari, posisi);
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Data keseluruhan Mahasiswa setelah dilakukan sorting :");
+        data.insertionSort();
+        data.tampil();
+
+        System.out.println("___________________");
+        System.out.println("___________________");
+        System.out.println("Pencarian Data :");
+        System.out.println("Masukkan NIM mahasiswa yang dicari :");
+        System.out.print("NIM : ");
+        int cari = s.nextInt();
+
+        System.out.println("Menggunakan sequential search");
+        int posisi = data.findSeqSearch(cari);
+        data.tampilPosisi(cari, posisi);
+        data.tampilData(cari, posisi);
+
+        System.out.println("Menggunakan binary search");
+        posisi = data.findBinarySearch(cari, 0,
+                data.pjgArr - 1);
+        data.tampilPosisi(cari, posisi);
+        data.tampilData(cari, posisi);
+    }
         }
     
 }
